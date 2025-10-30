a1e37f1310a1edc9b7b00d15b10bb8bc4376fd25
YoupWarmerdam <youp.warmerdam@student.han.nl>
2025-10-30 21:15:44 +0100

    Finished the generator
    - Generator implements all requirements from ASSIGNMENT.md (GE01 and GE02)
    - Updated progress tracking in README

--- README.md
+++ README.md
@@ -43,15 +43,15 @@ This tutorial is tested with Java version 17 (OpenJDK), IntelliJ and Maven. To e
 ### 4.5 Genereren (10 punten)
 | ID | Omschrijving | Prio | Punten | Status |
 |----|--------------|------|--------|--------|
-| GE01 | Implementeer de generator in `nl.han.ica.icss.generator.Generator` die de AST naar een CSS2-compliant string omzet. | Must | 5 | ⏳ |
-| GE02 | Zorg dat de CSS met twee spaties inspringing per scopeniveau gegenereerd wordt. | Must | 5 | ⏳ |
+|| GE01 | Implementeer de generator in `nl.han.ica.icss.generator.Generator` die de AST naar een CSS2-compliant string omzet. | Must | 5 | ✅ |
+|| GE02 | Zorg dat de CSS met twee spaties inspringing per scopeniveau gegenereerd wordt. | Must | 5 | ✅ |
 
 ### 4.6 Eigen Uitbreidingen (20 punten)
 | Omschrijving | Punten | Status |
 |--------------|--------|--------|
 | TBD - Af te spreken met docent | Max 20 | ⏳ |
 
-**Huidige stand: 90/120 punten**
+**Huidige stand: 100/120 punten**
 
 ## Running ICSSTool
 ICSSTool is a `pom.xml` based, Maven-runnable application.
--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,15 +1,68 @@
 package nl.han.ica.icss.generator;
 
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
+import nl.han.ica.icss.ast.selectors.*;
 
-import nl.han.ica.icss.ast.AST;
+import java.util.List;
 
 public class Generator {
 
 	public String generate(AST ast) {
-        return "";
-
-
+        return generateStylesheet(ast.root);
 	}
 
-	
+	private String generateStylesheet(Stylesheet sheet) {
+		StringBuilder stylesheet = new StringBuilder();
+		for (ASTNode child : sheet.getChildren()) {
+			if (child instanceof Stylerule) {
+				stylesheet.append(generateStylerule((Stylerule) child)).append("\n");
+			}
+		}
+		return stylesheet.toString();
+	}
+
+	private String generateStylerule(Stylerule rule) {
+		StringBuilder stylerule = new StringBuilder();
+		stylerule.append(generateSelector(rule.selectors));
+
+		for (ASTNode child : rule.body) {
+			if (child instanceof Declaration) {
+				stylerule.append("  ").append(generateDeclaration((Declaration) child)).append("\n");
+			}
+		}
+		return stylerule.append("}").toString();
+	}
+
+	private String generateSelector(List<Selector> selector) {
+		StringBuilder selectors = new StringBuilder();
+		for (Selector s : selector) {
+			if (s instanceof TagSelector) {
+				selectors.append(((TagSelector) s).tag);
+			} else if (s instanceof ClassSelector) {
+				selectors.append(((ClassSelector) s).cls);
+			} else if (s instanceof IdSelector) {
+				selectors.append(((IdSelector) s).id);
+			}
+		}
+		return selectors.append("{\n").toString();
+	}
+
+	private String generateDeclaration(Declaration declaration) {
+		return declaration.property.name + ": " + generateLiteral((Literal) declaration.expression) + ";";
+	}
+
+	private String generateLiteral(Literal expression) {
+		if (expression instanceof ColorLiteral) {
+			return ((ColorLiteral) expression).value;
+		} else if (expression instanceof PercentageLiteral) {
+			return ((PercentageLiteral) expression).value + "%";
+		} else if (expression instanceof PixelLiteral) {
+			return ((PixelLiteral) expression).value + "px";
+		} else if (expression instanceof ScalarLiteral) {
+			return String.valueOf(((ScalarLiteral) expression).value);
+		} else {
+			return null;
+		}
+	}
 }
